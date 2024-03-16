6329f022091b4c6e075ab3779c727e51bcb84ff4
arebelo <anagmrebelo@hotmail.com>
2024-03-16 16:15:54 +0100

    Feature: Implement logger in Menu

--- src/main/java/com/ironhack/ironlibrary/view/Menu.java
+++ src/main/java/com/ironhack/ironlibrary/view/Menu.java
@@ -1,4 +1,13 @@
+/* (C)2024 */
 package com.ironhack.ironlibrary.view;
 
+import com.ironhack.ironlibrary.IronlibraryApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Menu {
+    private static Logger LOG = LoggerFactory
+            .getLogger(Menu.class);
+    public Menu() {
+    }
 }

6d7d163a3c1ed0482b32220511eec1a0754a41fd
evagapi <87602505+evagapi@users.noreply.github.com>
2024-03-16 10:33:43 +0100

    chore: use spotless pluguin

--- src/main/java/com/ironhack/ironlibrary/IronlibraryApplication.java
+++ src/main/java/com/ironhack/ironlibrary/IronlibraryApplication.java
@@ -1,3 +1,4 @@
+/* (C)2024 */
 package com.ironhack.ironlibrary;
 
 import org.springframework.boot.SpringApplication;
@@ -6,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IronlibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IronlibraryApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(IronlibraryApplication.class, args);
+    }
 }

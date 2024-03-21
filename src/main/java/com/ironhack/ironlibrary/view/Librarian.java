3d8ba76f4169712670c82431a4550f2429b47a6e
ivandavid858 <ivandavid858@gmail.com>
2024-03-21 20:03:33 +0100

    Create method search book by title

--- src/main/java/com/ironhack/ironlibrary/view/Librarian.java
+++ src/main/java/com/ironhack/ironlibrary/view/Librarian.java
@@ -1,4 +1,27 @@
 /* (C)2024 */
 package com.ironhack.ironlibrary.view;
 
-public class Librarian {}
+import com.ironhack.ironlibrary.model.Book;
+import com.ironhack.ironlibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+@Component
+public class Librarian {
+
+    @Autowired
+    private BookService bookService;
+
+    public void searchBookByTitle() {
+        System.out.println("Enter a book title");
+        Scanner scanner = new Scanner(System.in);
+        String bookTitle = scanner.nextLine();
+        scanner.close();
+        Optional<Book> bookOptional = bookService.searchBookByTitle(bookTitle);
+        System.out.println(bookOptional.isPresent());
+    }
+
+}
---
+++ src/test/java/com/ironhack/ironlibrary/repository/BookRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.ironhack.ironlibrary.repository;
+
+import com.ironhack.ironlibrary.model.Book;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @BeforeEach
+    void setUp() {
+        List<Book> books = List.of(
+                new Book("Harry Potter and Philosopher Stone", "ABC159753", "Young adult", 10),
+                new Book("Final Empire", "DEF159753", "Fantasy", 8),
+                new Book("Oliver Twist", "GHI159753", "Fiction", 6)
+        );
+        bookRepository.saveAll(books);
+    }
+
+    @AfterEach
+    void tearDown() {
+        bookRepository.deleteAll();
+        bookRepository.flush();
+    }
+
+    @Test
+    public void testKoFindBookByTitle() {
+        Optional<Book> bookOptional = bookRepository.findBookByTitle("Matilda");
+        assertFalse(bookOptional.isPresent());
+    }
+    @Test
+    public void testOkFindBookByTitle() {
+        Optional<Book> bookOptional = bookRepository.findBookByTitle("Final Empire");
+        assertTrue(bookOptional.isPresent());
+        assertEquals("Final Empire", bookOptional.get().getTitle());
+    }
+
+}

018d6611494c24c092bc6a9cdf7542abafaf99f4
evagapi <eva.gapi@gmail.com>
2024-03-31 19:16:44 +0200

    feat: Add book (#6)
    * feat: create a Book
    
    * feat: allow user to add input in the same line of the prompt

--- src/main/java/com/ironhack/ironlibrary/view/Librarian.java
+++ src/main/java/com/ironhack/ironlibrary/view/Librarian.java
@@ -1,7 +1,10 @@
 /* (C)2024 */
 package com.ironhack.ironlibrary.view;
 
+import com.ironhack.ironlibrary.model.Author;
 import com.ironhack.ironlibrary.model.Book;
+import com.ironhack.ironlibrary.repository.AuthorRepository;
+import com.ironhack.ironlibrary.repository.BookRepository;
 import com.ironhack.ironlibrary.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,6 +17,10 @@ public class Librarian {
 
     @Autowired
     private BookService bookService;
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private AuthorRepository authorRepository;
 
     public void searchBookByTitle() {
         System.out.println("Enter a book title");
@@ -22,6 +29,27 @@ public class Librarian {
         scanner.close();
         Optional<Book> bookOptional = bookService.searchBookByTitle(bookTitle);
         System.out.println(bookOptional.isPresent());
+    }
+
+    public void addABook() {
+        System.out.print("Enter isbn : ");
+        Scanner scanner = new Scanner(System.in);
+        String isbn = scanner.nextLine();
+        System.out.print("Enter title : ");
+        String title = scanner.nextLine();
+        System.out.print("Enter category : ");
+        String category = scanner.nextLine();
+        System.out.print("Enter Author name : ");
+        String name = scanner.nextLine();
+        System.out.print("Enter Author mail : ");
+        String email = scanner.nextLine();
+        System.out.print("Enter number of books : ");
+        int quantity = Integer.parseInt(scanner.nextLine());
+
+        Book book = new Book(title, isbn, category, quantity);
+        bookRepository.save(book);
+        Author author = new Author(name, email, book);
+        authorRepository.save(author);
     }
 
 }

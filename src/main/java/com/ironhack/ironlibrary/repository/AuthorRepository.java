389794a582b911831c9d2eeb229ff1249ab03dc4
katalinjordans <katalinjordans@gmail.com>
2024-04-04 21:16:28 +0200

    feat: search book by author name logic

--- src/main/java/com/ironhack/ironlibrary/repository/AuthorRepository.java
+++ src/main/java/com/ironhack/ironlibrary/repository/AuthorRepository.java
@@ -2,8 +2,15 @@
 package com.ironhack.ironlibrary.repository;
 
 import com.ironhack.ironlibrary.model.Author;
+import com.ironhack.ironlibrary.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AuthorRepository extends JpaRepository<Author, Integer> {}
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    Optional<Author> findAuthorByName(String authorName);
+
+
+}
--- src/main/java/com/ironhack/ironlibrary/service/AuthorService.java
+++ src/main/java/com/ironhack/ironlibrary/service/AuthorService.java
@@ -2,11 +2,13 @@
 package com.ironhack.ironlibrary.service;
 
 import com.ironhack.ironlibrary.model.Author;
+import com.ironhack.ironlibrary.model.Book;
 import com.ironhack.ironlibrary.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AuthorService {
@@ -15,4 +17,9 @@ public class AuthorService {
     public List<Author> getAllAuthors() {
         return authorRepository.findAll();
     }
+
+    public Optional<Author> searchBookByAuthor(String authorName) {
+        return authorRepository.findAuthorByName(authorName);
+    }
+
 }
